fe81c86bcc9963c2027989cecd31370c80b186eb
israel.souza <israel.souza@summit-bra.com>
2022-09-08 09:57:43 -0300

    Server Streaming

--- greet/server/greet.go
+++ greet/server/greet.go
@@ -15,3 +15,18 @@ func (c *Server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetRespo
 
 	return &pb.GreetResponse{Result: fmt.Sprintf("Hello, %s", name)}, nil
 }
+
+func (c *Server) GreetManyTimes(in *pb.GreetRequest, stream pb.GreetService_GreetManyTimesServer) error {
+	log.Printf("GreetManyTimes function was invoked with: %v \n", in)
+
+	for i := 0; i < 10; i++ {
+		res := fmt.Sprintf("Hello %s, %d \n", in.FirstName, i)
+		err := stream.Send(&pb.GreetResponse{Result: res})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}

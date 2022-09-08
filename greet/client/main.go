fb8081c71a70f12be6e880a56b1eb66a4f0a34dc
israel.souza <israel.souza@summit-bra.com>
2022-09-08 12:41:31 -0300

    client

--- greet/client/main.go
+++ greet/client/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"math/rand"
 
 	pb "github.com/Israel-Ferreira/grpc-go-course/greet/proto"
 	"google.golang.org/grpc"
@@ -14,6 +16,49 @@ var addr string = "0.0.0.0:50051"
 
 type GreetClientService struct {
 	pb.GreetServiceClient
+}
+
+func doGreetManyTimes(c pb.GreetServiceClient) {
+	log.Printf("Make Request to gRPC server....")
+
+	names := []string{
+		"Israel",
+		"Nickolas",
+		"Milena",
+		"Ciro",
+		"Gabriel",
+		"Amanda",
+		"Marina",
+	}
+
+	randNum := rand.Intn(len(names))
+
+	fmt.Println(randNum)
+
+	req := &pb.GreetRequest{
+		FirstName: names[randNum],
+	}
+
+	stream, err := c.GreetManyTimes(context.Background(), req)
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	for {
+		msg, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		log.Printf("Received Message: %v \n", msg)
+	}
+
 }
 
 func main() {
@@ -28,7 +73,7 @@ func main() {
 
 	c := pb.NewGreetServiceClient(conn)
 
-	result, err  := c.Greet(context.Background(), &pb.GreetRequest{FirstName: "Israel"})
+	result, err := c.Greet(context.Background(), &pb.GreetRequest{FirstName: "Israel"})
 
 	if err != nil {
 		log.Fatalln(err.Error())
@@ -36,4 +81,5 @@ func main() {
 
 	fmt.Println(result)
 
+	doGreetManyTimes(c)
 }
